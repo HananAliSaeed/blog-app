35442ee0e0d6f84c8e1ed459a6abe7285ad0f817
Hanan <hananalisaeed7@gmail.com>
2023-11-26 22:07:16 +0300

    Testcase Update

--- tests/Feature/PostApiTest.php
+++ tests/Feature/PostApiTest.php
@@ -2,29 +2,75 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class PostApiTest extends TestCase
 {
-    use DatabaseTransactions;
-
-    public function setUp(): void
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testPostIndex()
     {
-        parent::setUp();
-        Artisan::call('migrate');
-        Artisan::call('db:seed', ['--class' => 'PostsTableSeeder']);
+        $headers = [
+            'Authorization' => '123456',
+        ];
+
+        $response = $this->withHeaders($headers)->get('api/posts');
+        $response->assertStatus(200);
     }
 
-    /** @test */
-    public function it_returns_all_posts()
+    public function testFetchBlogById()
     {
-        $response = $this->get('/api/posts');
+        $postId = 1;
+        $headers = [
+            'Authorization' => '123456',
+        ];
+        $response = $this->withHeaders($headers)->get("api/posts/{$postId}");
+        $response->assertStatus(200);
+    }
 
-        $response->assertStatus(200)
-            ->assertJsonCount(10);
+    public function testDeleteBlogById()
+    {
+        $postId = 2;
+        $headers = [
+            'Authorization' => '123456',
+        ];
+        $response = $this->withHeaders($headers)->delete("api/posts/{$postId}");
+        $response->assertStatus(200);
+    }
+
+    public function testStoreBlog()
+    {
+        $postData = [
+            'title' => 'Test Blog 100',
+            'content' => 'This is a test blog content. Lorem ipsum dolor sit amet.',
+            'user_id' => 100
+        ];
+        $headers = [
+            'Authorization' => '123456',
+        ];
+        $response = $this->withHeaders($headers)->post('api/posts', $postData);
+        $response->assertStatus(200);
+    }
+
+    public function testUpdateBlogById()
+    {
+        $postId = 1;
+
+        $postData = [
+            'title' => 'Updated Title',
+            'content' => 'Updated content for the blog post.',
+            'user_id' => 1
+        ];
+
+        $headers = [
+            'Authorization' => '123456',
+        ];
+        $response = $this->withHeaders($headers)->put("api/posts/{$postId}", $postData);
+        $response->assertStatus(200);
     }
 }
